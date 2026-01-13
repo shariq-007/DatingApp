ed1a655e01b991235f2064df13ffab3c024a4f32
shariq-007 <shariq_007@hotmail.com>
2026-01-13 07:33:09 +0500

    Section of section seven

---
+++ API/Errors/ApiException.cs
@@ -0,0 +1,9 @@
+namespace API.Errors;
+
+public class ApiException(int statuscode, string message, string? details)
+{
+    public int StatusCode { get; set; } = statuscode;
+    public string Message { get; set; } = message;
+    public string? Details { get; set; } = details;
+
+}
